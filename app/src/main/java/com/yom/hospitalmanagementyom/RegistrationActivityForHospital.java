ef1fe2ed2de8d4d30420284475bc5c4393460481
yousefShaaban42 <92793137+yousefShaaban42@users.noreply.github.com>
2021-11-27 19:14:55 +0200

    LoginActivity

---
+++ app/src/main/java/com/yom/hospitalmanagementyom/RegistrationActivityForHospital.java
@@ -0,0 +1,14 @@
+package com.yom.hospitalmanagementyom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationActivityForHospital extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_registration_for_hospital );
+    }
+}
