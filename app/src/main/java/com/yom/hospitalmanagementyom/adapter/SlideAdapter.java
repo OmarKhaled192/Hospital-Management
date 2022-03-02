d0b9dd0f57219a49aae3809a5288c91a98f95d3d
yousefShaaban42 <92793137+yousefShaaban42@users.noreply.github.com>
2022-03-02 16:32:41 +0200

    modify files

--- app/src/main/java/com/yom/hospitalmanagementyom/adapter/SlideAdapter.java
+++ app/src/main/java/com/yom/hospitalmanagementyom/adapter/SlideAdapter.java
@@ -1,14 +1,11 @@
 package com.yom.hospitalmanagementyom.adapter;
 
 
-import android.content.Context;
-import android.graphics.drawable.DrawableContainer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
