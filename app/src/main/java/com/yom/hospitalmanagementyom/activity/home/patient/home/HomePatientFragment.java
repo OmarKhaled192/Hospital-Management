125b9bc6b444ce8a56ae0156738e81ffa459b122
yousefShaaban42 <92793137+yousefShaaban42@users.noreply.github.com>
2022-03-27 00:55:00 +0200

    Update HospitalViewAdapter.java & item_hospital_view.xml & HomePatientFragment.java & item_post_for_home_patient.xml & colors.xml

--- app/src/main/java/com/yom/hospitalmanagementyom/activity/home/patient/home/HomePatientFragment.java
+++ app/src/main/java/com/yom/hospitalmanagementyom/activity/home/patient/home/HomePatientFragment.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -14,7 +12,6 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
 import com.facebook.shimmer.ShimmerFrameLayout;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -23,21 +20,25 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.yom.hospitalmanagementyom.R;
+import com.yom.hospitalmanagementyom.adapter.HospitalViewAdapter;
 import com.yom.hospitalmanagementyom.adapter.PostAdapter;
 import com.yom.hospitalmanagementyom.databinding.FragmentHomePatientBinding;
+import com.yom.hospitalmanagementyom.model.Hospital;
 import com.yom.hospitalmanagementyom.model.Post;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class HomePatientFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomePatientBinding binding;
+    private Hospital hospital;
+    private List<Hospital> hospitals;
+    private HospitalViewAdapter hospitalViewAdapter;
     private Post post;
     private List<Post> posts;
-    private PostAdapter postAdapter,postAdapter1;
+    private PostAdapter postAdapter;
     private RecyclerView recyclerViewHomePatient, recyclerViewHospitalView;
     private FirebaseFirestore firebaseFirestore;
     private String POSTS="Posts";
@@ -58,14 +59,23 @@ public class HomePatientFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         firebaseFirestore=FirebaseFirestore.getInstance();
 
-        post=new Post();
-        posts=new ArrayList<>();
-        postAdapter1=new PostAdapter(Objects.requireNonNull(getContext()), posts );
+        hospital=new Hospital();
+        hospital.setName("App Name ");
+        hospital.setProfile("link");
+        hospitals=new ArrayList<>();
+        hospitals.add(hospital);
+        hospitals.add(hospital);
+        hospitals.add(hospital);
+        hospitals.add(hospital);
+        hospitals.add(hospital);
+        hospitalViewAdapter=new HospitalViewAdapter(getContext(), hospitals );
         LinearLayoutManager linearLayoutManager2=new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL,false);
         recyclerViewHospitalView=binding.recyclerViewHospitalView ;
         recyclerViewHospitalView.setLayoutManager(linearLayoutManager2);
-        recyclerViewHospitalView.setAdapter(postAdapter1);
+        recyclerViewHospitalView.setAdapter(hospitalViewAdapter);
 
+        post=new Post();
+        posts=new ArrayList<>();
         postAdapter=new PostAdapter( getContext(), posts );
         LinearLayoutManager linearLayoutManager=new LinearLayoutManager( getContext() );
         recyclerViewHomePatient=binding.recyclerViewHomePatient ;
@@ -78,6 +88,8 @@ public class HomePatientFragment extends Fragment {
             @Override
             public void onRefresh() {
                 shimmerFrameLayout.setVisibility(View.VISIBLE);
+                recyclerViewHospitalView.setVisibility(View.GONE);
+                binding.viewHome.setVisibility(View.GONE);
                 recyclerViewHomePatient.setVisibility(View.GONE);
                 shimmerFrameLayout.showShimmer(true);
                 shimmerFrameLayout.startShimmer();
@@ -102,6 +114,7 @@ public class HomePatientFragment extends Fragment {
                 shimmerFrameLayout.hideShimmer();
                 shimmerFrameLayout.setVisibility(View.GONE);
                 recyclerViewHomePatient.setVisibility(View.VISIBLE);
+                binding.viewHome.setVisibility(View.VISIBLE);
                 recyclerViewHospitalView.setVisibility(View.VISIBLE);
                 postAdapter.notifyDataSetChanged();
                 swipeRefreshLayout.setRefreshing( false );
--- app/src/main/java/com/yom/hospitalmanagementyom/adapter/HospitalViewAdapter.java
+++ app/src/main/java/com/yom/hospitalmanagementyom/adapter/HospitalViewAdapter.java
@@ -1,4 +1,56 @@
 package com.yom.hospitalmanagementyom.adapter;
 
-public class HospitalViewAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+import com.yom.hospitalmanagementyom.R;
+import com.yom.hospitalmanagementyom.model.Hospital;
+import java.util.List;
+
+public class HospitalViewAdapter extends RecyclerView.Adapter<HospitalViewAdapter.HospitalHolder> {
+    private final Context context;
+    private final List<Hospital> hospitals;
+
+    public HospitalViewAdapter(Context context, List<Hospital> hospitals){
+        this.context=context;
+        this.hospitals=hospitals;
+    }
+
+    @NonNull
+    @Override
+    public HospitalHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_hospital_view,parent,false);
+        return new HospitalHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HospitalHolder holder, int position) {
+        Hospital hospital=hospitals.get(position);
+        holder.nameHospital.setText(hospital.getName());
+        Picasso.with(context).load(hospital.getProfile()).error(R.color.white).into(holder.profileHospital);
+    }
+
+    @Override
+    public int getItemCount() {
+        return hospitals.size();
+    }
+
+
+    static class HospitalHolder extends RecyclerView.ViewHolder{
+        ImageView profileHospital;
+        TextView nameHospital;
+        public HospitalHolder(@NonNull View itemView) {
+            super(itemView);
+            profileHospital=itemView.findViewById(R.id.profileHospital);
+            nameHospital=itemView.findViewById(R.id.nameHospital);
+        }
+    }
 }
