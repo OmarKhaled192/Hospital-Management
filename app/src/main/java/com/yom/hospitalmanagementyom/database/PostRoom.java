20b31e60f4e276876f23162984a11ced5002d254
yousefShaaban42 <92793137+yousefShaaban42@users.noreply.github.com>
2022-03-28 19:35:06 +0200

    update PostRoom.java

--- app/src/main/java/com/yom/hospitalmanagementyom/database/PostRoom.java
+++ app/src/main/java/com/yom/hospitalmanagementyom/database/PostRoom.java
@@ -1,60 +1,52 @@
 package com.yom.hospitalmanagementyom.database;
 
 import android.content.Context;
-import android.os.AsyncTask;
-
 import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
-
 import com.yom.hospitalmanagementyom.model.Post;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Database(entities = Post.class, version = 1)
 public abstract class PostRoom extends RoomDatabase {
-    private static volatile PostRoom instance;
     public abstract PostDao postDao();
-    private static final String DATABASE_NAME="Hospital Management" ;
 
-    public static synchronized PostRoom getInstance(Context context){
-        if(instance!=null){
-            instance= Room.databaseBuilder(context.getApplicationContext(),PostRoom.class,
-                    DATABASE_NAME).fallbackToDestructiveMigration()
-                    .addCallback(roomCallback).build();
+    private static volatile PostRoom INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+    private static final String DATABASE_NAME="Hospital Management" ;
+    static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    static PostRoom getInstance(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (PostRoom.class) {
+                if (INSTANCE == null) {
+                    INSTANCE= Room.databaseBuilder(context.getApplicationContext(),
+                            PostRoom.class, DATABASE_NAME)
+                            .fallbackToDestructiveMigration()
+                            .addCallback(roomCallback).build();
+                }
+            }
         }
-        return instance;
+        return INSTANCE;
     }
 
-    private static final RoomDatabase.Callback roomCallback=new RoomDatabase.Callback(){
+    private static final RoomDatabase.Callback roomCallback=new RoomDatabase.Callback() {
         @Override
         public void onCreate(@NonNull SupportSQLiteDatabase db) {
             super.onCreate(db);
-            new PopulateDataAsyncTask(instance).execute();
-        }
+            databaseWriteExecutor.execute(() -> {
+                PostDao postDao = INSTANCE.postDao();
+                Post post = new Post();
+                post.setPost("JO");
+                post.setId("0");
+                post.setName("UO");
 
-        @Override
-        public void onOpen(@NonNull SupportSQLiteDatabase db) {
-            super.onOpen(db);
+                postDao.insert(post);
+            });
         }
     };
-
-    private static class PopulateDataAsyncTask extends AsyncTask<Void,Void,Void>{
-
-        private final PostDao postDao;
-        public PopulateDataAsyncTask( PostRoom db){
-            postDao= db.postDao();
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            Post post=new Post();
-            post.setPost("JO");
-            post.setId("0");
-            post.setName("UO");
-
-            postDao.insert(post);
-            return null;
-        }
-    }
 }
