3d8063b12c23b29d6b5b2de36bdd164b86f83ab5
YousefShaabanSaad <101521680+YousefShaabanSaad@users.noreply.github.com>
2022-04-26 09:31:14 +0200

    update ChooseActivity

--- app/src/main/java/com/yom/hospitalmanagementyom/activity/registration/ChooseActivity.java
+++ app/src/main/java/com/yom/hospitalmanagementyom/activity/registration/ChooseActivity.java
@@ -1,11 +1,8 @@
 package com.yom.hospitalmanagementyom.activity.registration;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import com.yom.hospitalmanagementyom.R;
 import com.yom.hospitalmanagementyom.databinding.ActivityChooseBinding;
 
 
@@ -16,18 +13,15 @@ public class ChooseActivity extends AppCompatActivity {
         super.onCreate( savedInstanceState );
         ActivityChooseBinding binding= ActivityChooseBinding.inflate(getLayoutInflater());
         setContentView( binding.getRoot() );
-        setSupportActionBar(binding.toolbar);
-        binding.toolbar.setNavigationIcon(R.drawable.back);
-        binding.toolbar.setNavigationOnClickListener(view -> onBackPressed());
-    }
 
-    public void registrationForHospital(View view) {
-        Intent intent=new Intent(getBaseContext(), RegistrationActivityForHospital.class);
-        startActivity( intent );
-    }
+        binding.registrationForPatient.setOnClickListener(view -> {
+            Intent intent=new Intent(getBaseContext(), RegistrationActivity.class);
+            startActivity( intent );
+        });
 
-    public void registrationForPatient(View view) {
-        Intent intent=new Intent(getBaseContext(), RegistrationActivity.class);
-        startActivity( intent );
+        binding.registrationForHospital.setOnClickListener(view -> {
+            Intent intent=new Intent(getBaseContext(), RegistrationActivityForHospital.class);
+            startActivity( intent );
+        });
     }
 }
