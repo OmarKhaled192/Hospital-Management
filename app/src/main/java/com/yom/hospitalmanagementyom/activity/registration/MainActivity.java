df5cffea4d7381b637cd525a42fd601533291730
YousefShaabanSaad <101521680+YousefShaabanSaad@users.noreply.github.com>
2022-05-13 16:22:41 +0200

    update MainActivity

--- app/src/main/java/com/yom/hospitalmanagementyom/activity/registration/MainActivity.java
+++ app/src/main/java/com/yom/hospitalmanagementyom/activity/registration/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
             }
-        };
+        }.start();
     }
 
     void checkUser(){
