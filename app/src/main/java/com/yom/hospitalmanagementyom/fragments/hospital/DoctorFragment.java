37c427039977df30040e4b3f6e6d077eba040141
YousefShaabanSaad <101521680+YousefShaabanSaad@users.noreply.github.com>
2022-05-22 18:13:29 +0200

    update1

--- app/src/main/java/com/yom/hospitalmanagementyom/fragments/hospital/DoctorFragment.java
+++ app/src/main/java/com/yom/hospitalmanagementyom/fragments/hospital/DoctorFragment.java
@@ -6,16 +6,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import com.yom.hospitalmanagementyom.databinding.FragmentDoctorBinding;
 
 public class DoctorFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
-
+    private FragmentDoctorBinding binding;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
-
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentDoctorBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
