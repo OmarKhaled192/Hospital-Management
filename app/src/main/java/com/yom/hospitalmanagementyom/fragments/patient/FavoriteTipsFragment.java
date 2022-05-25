7d31f55b8a0df3c97144defa2cc0c6bafe14e9e9
YousefShaabanSaad <101521680+YousefShaabanSaad@users.noreply.github.com>
2022-05-25 08:30:33 +0200

    update Register

--- app/src/main/java/com/yom/hospitalmanagementyom/fragments/patient/FavoriteTipsFragment.java
+++ app/src/main/java/com/yom/hospitalmanagementyom/fragments/patient/FavoriteTipsFragment.java
@@ -8,6 +8,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.yom.hospitalmanagementyom.adapter.PostAdapter;
 import com.yom.hospitalmanagementyom.database.Repository;
 import com.yom.hospitalmanagementyom.databinding.FragmentFavoriteTipsBinding;
@@ -30,7 +35,6 @@ public class FavoriteTipsFragment extends Fragment implements PostsListener {
 
         repository=new Repository(requireContext());
         //posts = repository.getPostsStarted(this);
-        posts = repository.getPosts(this);
     }
 
 
@@ -43,10 +47,24 @@ public class FavoriteTipsFragment extends Fragment implements PostsListener {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        postAdapter = new PostAdapter(requireContext(), posts, this);
-        LinearLayoutManager linearLayoutManager=new LinearLayoutManager( requireContext() );
-        binding.recyclerviewFavoriteTips.setLayoutManager( linearLayoutManager );
-        binding.recyclerviewFavoriteTips.setAdapter(postAdapter);
+        FirebaseDatabase.getInstance().getReference(Constants.POSTS).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot d : dataSnapshot.getChildren()) {
+                    Post post = d.getValue(Post.class);
+                    posts.add(post);
+                }
+                posts.remove(2);
+                posts.remove(1);
+                postAdapter = new PostAdapter(requireContext(), posts, FavoriteTipsFragment.this);
+                LinearLayoutManager linearLayoutManager=new LinearLayoutManager( requireContext() );
+                binding.recyclerviewFavoriteTips.setLayoutManager( linearLayoutManager );
+                binding.recyclerviewFavoriteTips.setAdapter(postAdapter);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
     }
 
 
