dba986c7709d199eddf2aaeed76ffa2b1356fe2a
YousefShaabanSaad <101521680+YousefShaabanSaad@users.noreply.github.com>
2022-05-25 08:15:43 +0200

    update Register

--- app/src/main/java/com/yom/hospitalmanagementyom/fragments/patient/ChatFragment.java
+++ app/src/main/java/com/yom/hospitalmanagementyom/fragments/patient/ChatFragment.java
@@ -5,13 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
-
 import com.google.firebase.database.FirebaseDatabase;
 import com.yom.hospitalmanagementyom.activity.home.patient.MessageActivity;
 import com.yom.hospitalmanagementyom.adapter.ChatAdapter;
